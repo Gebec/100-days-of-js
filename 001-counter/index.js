86de427be962a518f7fbc442437e3be1685b2ab0
gebec <gebec.m@gmail.com>
2023-05-21 23:42:25 +0200

    Adds 001 counter

---
+++ 001-counter/index.js
@@ -0,0 +1,40 @@
+const init = (() => {
+    const add = document.getElementById('button-add');
+    const reset = document.getElementById('button-reset');
+    const subtract = document.getElementById('button-subtract');
+    const countElement = document.getElementById('count');
+
+    let stateValue = 0
+
+    const updateCount = () => {
+        countElement.innerText = stateValue;
+        if (stateValue === 0) {
+            countElement.style.color = "white";
+            return;
+        }
+
+        if (stateValue > 0) {
+            countElement.style.color = "LightGreen";
+            return;
+        }
+
+        countElement.style.color = "tomato";
+    }
+
+    add.addEventListener('click', () => {
+        stateValue += 1;
+        updateCount();
+    });
+
+    reset.addEventListener('click', () => {
+        stateValue = 0;
+        updateCount();
+    })
+
+    subtract.addEventListener('click', () => {
+        stateValue -= 1;
+        updateCount();
+    })
+
+    updateCount();
+})();
---
+++ readme.md
@@ -0,0 +1,3 @@
+Inspired by [100 days of code](https://www.udemy.com/course/100-days-of-javascript)
+
+Personally used to learn new frameworks, libraries and approaches.
