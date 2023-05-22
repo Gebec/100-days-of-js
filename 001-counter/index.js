7a2b0db7e3facbe8d6c03500c424213cc193283c
gebec <gebec.m@gmail.com>
2023-05-22 22:01:27 +0200

    Minor fix

--- 001-counter/index.js
+++ 001-counter/index.js
@@ -1,40 +1,38 @@
-const init = (() => {
-    const add = document.getElementById('button-add');
-    const reset = document.getElementById('button-reset');
-    const subtract = document.getElementById('button-subtract');
-    const countElement = document.getElementById('count');
+const add = document.getElementById('button-add');
+const reset = document.getElementById('button-reset');
+const subtract = document.getElementById('button-subtract');
+const countElement = document.getElementById('count');
 
-    let stateValue = 0
+let stateValue = 0
 
-    const updateCount = () => {
-        countElement.innerText = stateValue;
-        if (stateValue === 0) {
-            countElement.style.color = "white";
-            return;
-        }
-
-        if (stateValue > 0) {
-            countElement.style.color = "LightGreen";
-            return;
-        }
-
-        countElement.style.color = "tomato";
+const updateCount = () => {
+    countElement.innerText = stateValue;
+    if (stateValue === 0) {
+        countElement.style.color = "white";
+        return;
     }
 
-    add.addEventListener('click', () => {
-        stateValue += 1;
-        updateCount();
-    });
+    if (stateValue > 0) {
+        countElement.style.color = "LightGreen";
+        return;
+    }
 
-    reset.addEventListener('click', () => {
-        stateValue = 0;
-        updateCount();
-    })
+    countElement.style.color = "tomato";
+}
 
-    subtract.addEventListener('click', () => {
-        stateValue -= 1;
-        updateCount();
-    })
-
+add.addEventListener('click', () => {
+    stateValue += 1;
     updateCount();
-})();
+});
+
+reset.addEventListener('click', () => {
+    stateValue = 0;
+    updateCount();
+})
+
+subtract.addEventListener('click', () => {
+    stateValue -= 1;
+    updateCount();
+})
+
+updateCount();
